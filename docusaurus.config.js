b9fe8ae2f0277e0c4285c00ef4893e752966d30d
Topasm <dhkdwk0215@naver.com>
2023-09-25 17:05:12 +0900

    plz

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,13 @@ const config = {
   url: 'https://mju-capstone-petrobot.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/mjbot_docs/',
+
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'mju-capstone-petrobot', // Usually your GitHub org/user name.
+  projectName: 'mjbot_docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',

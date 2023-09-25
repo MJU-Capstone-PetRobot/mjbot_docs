45a9e05d254dbd93dfe46f63482c9af03e8b5286
Kim Seong Hyeon <dhkdwk0215@naver.com>
2023-09-25 18:18:44 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://mju-capstone-petrobot.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/mjbot_docs/',
+  baseUrl: '/',
 
 
   // GitHub pages deployment config.
